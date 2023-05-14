ef121f30d6bb72bf1327cd919b6b861b66a4e50b
xXhemajl <115585034+xXhemajl@users.noreply.github.com>
2023-05-14 18:59:18 +0200

    Update Ceasar and Vigenere Cipher.py

--- Ceasar and Vigenere Cipher.py
+++ Ceasar and Vigenere Cipher.py
@@ -94,6 +94,48 @@ def ceasarCypher(message,key,encrypt) -> str: #Remember to remove message
 
     return output
 
+   def vigenereCypher(message,phrase,encrypt) -> str:
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    i = 0
+    output = ""
+    message = message.upper()
+    phrase =  phrase.upper()
+    for symbol in message:
+        if i == len(phrase):
+            i = i - len(phrase)
+        if symbol in alphabet:
+            num = alphabet.find(symbol)
+            shiftFactor = alphabet.find(phrase[i])
+            if encrypt == True:
+                num = num + shiftFactor
+            else:
+                num = num - shiftFactor
+            ## Wrap-around protection
+            if num >= len(alphabet):
+                num = num - len(alphabet)
+            elif num < 0:
+                num = num + len(alphabet)
+            i = i + 1
+            ## Adding generated letter to translated array
+            output = output + alphabet[num]
+        else:
+            output = output + symbol ## If symbol is not recognised as a letter, it is unaltered
+    return output
+
+def encrypt():
+    cypher=mode.get()
+    if cypher == "ceasar":
+        key=int(ent_key.get())
+        message=txt_edit.get("1.0", tk.END)
+        translate=ceasarCypher(message,key,encrypt=True)
+        txt_edit.delete("1.0", tk.END)
+        txt_edit.insert(tk.END, translate)
+    if cypher == "vigenere":
+        phrase=str(ent_phrase.get())
+        message=txt_edit.get("1.0", tk.END)
+        translate=vigenereCypher(message,phrase,encrypt=True)
+        txt_edit.delete("1.0", tk.END)
+        txt_edit.insert(tk.END, translate)
 
 ##Erblini
 
